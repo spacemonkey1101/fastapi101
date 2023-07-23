fccf77aac6210b5c2d689eb21a22d0fa80181faf
spacemonkey1101 <ritambh54@gmail.com>
2023-07-23 17:59:14 +0530

    automatic JSON convertion by fastapi

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ PUT , DELETE
 def home():
     return {"Data":"Testing"}
 #when we run our webserver and hit the root endpoint, it will return us the dictionary
+# as soon as the function returns the dictionary is converted to a JSON
+# this is a easier way for HTTP traffic to exchange
+# fastapi handles JSONifying all of our data, so that we can work with python types
+# same happens we receive JSON data, fast api converts it to python types for us to work with
+
 
 '''Running the webserver  
 >uvicorn main:app --reload
