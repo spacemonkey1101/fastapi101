a230467edb8e08c57fab577a8d355c21acda86e0
spacemonkey1101 <ritambh54@gmail.com>
2023-07-23 14:51:26 +0530

    setting up root GET endpoint | testing

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-import fastapi
+from fastapi import FastAPI
 
+#creates an API object
+app = FastAPI() 
+
+'''
+GET - endpoint that returns information
+POST - sending/adding data to the enpoint
+PUT , DELETE
+'''
+#setting up root GET endpoint
+@app.get("/")
+def home():
+    return {"Data":"Test"}
+#when we run our webserver and hit the root endpoint, it will return us the dictionary
